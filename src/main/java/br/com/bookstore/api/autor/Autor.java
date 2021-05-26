8104443cb03f339a19abe63940ee32cc8d041e8c
brayanbertan <brayanbertan@gmail.com>
2021-05-26 19:33:16 -0300

    inicio crud java

--- src/main/java/br/com/bookstore/api/autor/Autor.java
+++ src/main/java/br/com/bookstore/api/autor/Autor.java
@@ -5,6 +5,7 @@
  */
 package br.com.bookstore.api.autor;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 /**
@@ -14,6 +15,56 @@ import java.util.Date;
 public class Autor {
     private int id;
     private String nome;
-    private Date dataNascimento;
+    private LocalDate dataNascimento;
     private Sexo sexo;
+
+    public Autor() {
+    }
+
+    public Autor(String nome, LocalDate dataNascimento, Sexo sexo) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.sexo = sexo;
+    }
+
+    public Autor(int id, String nome, LocalDate dataNascimento, Sexo sexo) {
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.sexo = sexo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+    
+    
 }
--- src/main/java/br/com/bookstore/api/autor/AutorResource.java
+++ src/main/java/br/com/bookstore/api/autor/AutorResource.java
@@ -5,13 +5,69 @@
  */
 package br.com.bookstore.api.autor;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.stream.Collectors;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
 
 /**
  *
  * @author brayanbertan
  */
-@Path("autores/")
+@Path("autores")
 public class AutorResource {
+    static DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+    static List<Autor> autores = new ArrayList<>(
+              Arrays.asList(
+                new Autor(1,"abc",LocalDate.of(1998,Month.MAY,1),Sexo.M)
+                 )
+            );
+    
+ 
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Autor> getAutores(){
+        return autores;
+    }
+    
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Autor getAutor(@PathParam("id") int id){
+       Autor autor =  autores.stream()
+                .filter(a -> a.getId() == id)
+                .findFirst()
+                .orElse(null);
+       if(autor == null){
+            throw new WebApplicationException("Autor não encontrando",Response.Status.NOT_FOUND);
+       }
+       return autor;
+    }
+    
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Autor setAutor(Autor autor){
+        int id = autores.get(autores.size()-1).getId();
+        autor.setId(++id);
+        this.autores.add(autor);
+        return autor;
+    }
     
 }
